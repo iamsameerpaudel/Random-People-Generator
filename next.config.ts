11b84cb265aaf2c515bfc795e9fdd1ae2a5b79f1
Sameer Paudel <samir.poudel@truenary.com>
2025-06-14 21:35:57 +0545

    init: initialise and cleanup an empty NextJs project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['randomuser.me'],
 
-const nextConfig: NextConfig = {
-  /* config options here */
+  },
+  devIndicators: false,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
